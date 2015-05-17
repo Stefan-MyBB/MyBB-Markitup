5dd5ad821c25ad5b22a9a17cc96acae944813330
Stefan-ST <mybbcms@yahoo.de>
2015-05-17 21:29:34 +0200

    Version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # mybb-markitup
 markItUp! editor for MyBB 1.8
+
+## Installation
+Upload files in "Upload" and activate plugin.
+
+## Credits
+markItUp! (http://markitup.jaysalvat.com/) dual licensed under MIT and GPL licenses.
+Icons taken from Silk Icons (http://www.famfamfam.com/lab/icons/silk/) licensed under Creative Commons Attribution 3.0 License.
---
+++ Upload/inc/languages/english/markitup.lang.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * This file is part of markItUp! for MyBB.
+ * Copyright (C) 2015 StefanT (https://www.mybb.de)
+ * https://github.com/Stefan-ST/MyBB-Markitup
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+$l['editor_title_bold'] = "Insert bold text";
+$l['editor_title_italic'] = "Insert italic text";
+$l['editor_title_underline'] = "Insert underlined text";
+$l['editor_title_left'] = "Align text to the left";
+$l['editor_title_center'] = "Align text to the center";
+$l['editor_title_right'] = "Align text to the right";
+$l['editor_title_justify'] = "Justify text";
+$l['editor_title_numlist'] = "Insert numbered list";
+$l['editor_title_bulletlist'] = "Insert bulleted list";
+$l['editor_title_image'] = "Insert image";
+$l['editor_title_hyperlink'] = "Insert hyperlink";
+$l['editor_title_email'] = "Insert email address";
+$l['editor_title_quote'] = "Insert quoted text";
+$l['editor_title_code'] = "Insert formatted code";
+$l['editor_title_php'] = "Insert formatted PHP code";
+$l['editor_enter_list_item'] = "Enter a list item. Click cancel or leave blank to end the list.";
+$l['editor_enter_url'] = "Please enter the URL of the website.";
+$l['editor_enter_url_title'] = "Optionally, you can also enter a title for the URL.";
+$l['editor_enter_email'] = "Please enter the email address you wish to insert.";
+$l['editor_enter_email_title'] = "Optionally, you may also enter a title for the email address.";
+$l['editor_enter_image'] = "Please enter the remote URL of the image you wish to insert.";
+$l['editor_enter_video_url'] = "Please enter the URL of the video.";
+$l['editor_video_dailymotion'] = "Dailymotion";
+$l['editor_video_facebook'] = "Facebook";
+$l['editor_video_liveleak'] = "LiveLeak";
+$l['editor_video_metacafe'] = "MetaCafe";
+$l['editor_video_veoh'] = "Veoh";
+$l['editor_video_vimeo'] = "Vimeo";
+$l['editor_video_yahoo'] = "Yahoo Video";
+$l['editor_video_youtube'] = "YouTube";
+$l['editor_size_xx_small'] = "XX Small";
+$l['editor_size_x_small'] = "X Small";
+$l['editor_size_small'] = "Small";
+$l['editor_size_medium'] = "Medium";
+$l['editor_size_large'] = "Large";
+$l['editor_size_x_large'] = "X Large";
+$l['editor_size_xx_large'] = "XX Large";
+$l['editor_font'] = "Font";
+$l['editor_size'] = "Text Size";
+$l['editor_color'] = "Text Color";
